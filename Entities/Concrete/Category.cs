84cfe6f2002633c4b01239cae256f38fb96fd33a
rknyprk2001@gmail.com <rknyprk2001@gmail.com>
2021-02-25 14:53:17 +0300

    deneme

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Çıplak sınıf kalmasın :)
+    public class Category:IEntity
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
